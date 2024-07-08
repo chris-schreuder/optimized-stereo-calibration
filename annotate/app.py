dc902be1cca42ba4cf05bf109fa43e9470ff6a5e
Chris Schreuder <chrismari.schreuder@gmail.com>
2024-07-09 00:30:51 +0200

    added eval annotation
    Manual annotation code flask app added

---
+++ annotate/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+all_points = []
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route to handle mouse click coordinates
+@app.route('/log_click', methods=['POST'])
+def log_click():
+    data = request.json
+    x = data.get('x')
+    y = data.get('y')
+    print(f"({x}, {y})")  # You can log this to a file or database
+    all_points.append((x, y))
+    print(all_points)
+    return jsonify(success=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
